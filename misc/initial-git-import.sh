487cabffe6d6a87f376a77c524ba97a0d15ce4ee
Stefano Zacchiroli <zack@upsilon.cc>
2018-03-25 10:02:17 -0400

    check-in original git migration script
    just for historical reasons, nothing to see here

---
+++ misc/initial-git-import.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# ledger2beancount started as an ad hoc personal script. As such it was
+# maintained in zack's private Ledger Git repository. It was later spinned off
+# as an indepent project, when tbm started to heavily contribute to it. The
+# script below was used to migrate the git history of ledger2beancount from
+# zack's private repo to a new, independent one. It is ad hoc, uninteresting,
+# and archived here just for historical reasons.
+
+flags="--force --prune-empty"
+revs="-- --all"
+git_url="git@*censored*:ledger"
+
+git clone "$git_url" ledger2beancount
+cd ledger2beancount
+
+git filter-branch $flags --subdirectory-filter bin $revs
+git filter-branch $flags --tree-filter "ls | grep -v beancount | xargs rm" $revs
+git filter-branch $flags --env-filter '
+  export GIT_AUTHOR_NAME="Martin Michlmayr"
+  export GIT_AUTHOR_EMAIL="tbm@cyrius.com"
+  export GIT_COMMITTER_NAME="Stefano Zacchiroli"
+  export GIT_COMMITTER_EMAIL="zack@upsilon.cc"
+' -- $(git rev-list HEAD --since=2018-03-10 | tail -n 1)^..HEAD
