a64a759864c2635c4d295d3ef203841ef8e54730
Martin Michlmayr <tbm@cyrius.com>
2018-05-08 22:14:34 +0200

    tests: run tests on Windows with AppVeyor

---
+++ tests/runtests.bat
@@ -0,0 +1,34 @@
+
+setlocal enableDelayedExpansion
+
+:: Set UTF-8 locale
+chcp 65001
+
+:: Convert ledger files
+set error=0
+for /r %%i in (*.ledger) do (
+    echo Converting %%i...
+    set ledger=%%i
+    set beancount=!ledger:.ledger=.beancount!
+    set yml=!ledger:.ledger=.yml!
+    if exist !yml! (
+        set config=!yml!
+    ) else (
+        set config=ledger2beancount.yml
+    )
+    perl ..\bin\ledger2beancount --config !config! %%i > temp
+    diff --strip-trailing-cr -urN !beancount! temp
+    if errorlevel 1 set error=1
+    del temp
+)
+if "%error%"=="1" (
+    echo One or more conversions had unexpected results
+    exit /b 1
+)
+
+:: Validate beancount files
+for /r %%i in (*.beancount) do (
+    echo Validating %%i...
+    bean-check %%i
+)
+
