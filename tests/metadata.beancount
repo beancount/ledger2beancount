beancount/ledger2beancount

commit eedcb8b6b24811c924ba3a4c8197387f36060653

Handle empty metadata values
It's possible to set metadata tags with an empty value: use a colon after
the key with nothing after or trailing whitespace:

    2020-02-18 * Empty (but defined tag)
        ; Check_Memo:

This should be converted to a metadata in beancount with an empty string
instead of as a comment.

Fixes #183

bin/ledger2beancount
@@ -97,7 +97,7 @@ my $amount_mcn_RE = qr/(?<minus>-?)(?<commodity>$commodity_RE)\s*(?<num>$value_e
 my $amount_RE = qr/(?<inline_math>\(?)($amount_mnc_RE|$amount_cmn_RE|$amount_mcn_RE)(\)?)/;
 my $comment_top_level_RE = qr/[;#%|*]\s?(?<comment>.*?)/;
 my $comment_RE = qr/;\s*(?<comment>.*)/;
-my $metadata_RE = qr/;\s*(?<key>[^\h:][^\h]*?):(?<typed>:)?\s+(?<value>.*)/;
+my $metadata_RE = qr/;\s*(?<key>[^\h:][^\h]*?):(?<typed>:)?(\s*$|\s+(?<value>.*))/;
 my $hledger_metadata_RE = qr/(?<key>[^:]+):\s*(?<value>.+)/;
 my $posting_RE = qr/(?<posting>((?<flag>$flags_RE)\s+)?(?<account>$account_RE)(  | \t|\t|\s*$)\s*(?<amount>$amount_RE)?(?<datestrip>\s*;\s*\[(?<postdate>$date_RE)?(=(?<auxdate>$date_RE))?\])?)/;
 my $price_RE = qr/^P\s+(?<date>$date_RE)\s+(\d\d:\d\d(:\d\d)?\s+)?(?<commodity1>$commodity_RE)\s+(?<commodity2>.*)/;
@@ -418,11 +418,12 @@ sub handle_metadata($$$) {
 	if (lc $key ~~ [ map lc $_, @{$config->{link_tags}} ] && !$in_postings) {
 	    print_tags $depth, "^$metadata->{value}";
 	} else {
-	    my $value;
+	    # Metadata values can be empty
+	    my $value = $metadata->{value} ? $metadata->{value} : "";
 	    if (defined($metadata->{typed})) {
-		$value = parse_ledger_value $metadata->{value};
+		$value = parse_ledger_value $value;
 	    } else {
-		$value = mk_beancount_string $metadata->{value};
+		$value = mk_beancount_string $value;
 	    }
 	    if ($defer) {
 		push @cur_txn_meta, pp_metadata($key, $value);

tests/metadata.beancount
@@ -140,3 +140,18 @@
     ax: "books"
   Assets:Test                        -10.00 EUR
 
+2020-02-18 * "Empty (but defined tag)"
+  check_memo: ""
+  Assets:Test                        10.00 EUR
+  Equity:Opening-Balance            -10.00 EUR
+
+2020-02-18 * "Empty (but defined tag) with trailing whitespace"
+  check_memo: ""
+  Assets:Test                        10.00 EUR
+  Equity:Opening-Balance            -10.00 EUR
+
+2020-02-18 * "No space -> not recognized by ledger as metadata"
+  ; check_memo:bar
+  Assets:Test                        10.00 EUR
+  Equity:Opening-Balance            -10.00 EUR
+

tests/metadata.ledger
@@ -121,3 +121,18 @@ commodity EUR
    ; a: books
    Assets:Test                        -10.00 EUR
 
+2020-02-18 * Empty (but defined tag)
+    ; check_memo:
+    Assets:Test                        10.00 EUR
+    Equity:Opening-Balance            -10.00 EUR
+
+2020-02-18 * Empty (but defined tag) with trailing whitespace
+    ; check_memo:  
+    Assets:Test                        10.00 EUR
+    Equity:Opening-Balance            -10.00 EUR
+
+2020-02-18 * No space -> not recognized by ledger as metadata
+    ; check_memo:bar
+    Assets:Test                        10.00 EUR
+    Equity:Opening-Balance            -10.00 EUR
+

.editorconfig
@@ -36,6 +36,9 @@ trim_trailing_whitespace = false
 [tests/hledger.beancount]
 trim_trailing_whitespace = false
 
+[tests/metadata.ledger]
+trim_trailing_whitespace = false
+
 [tests/metadata.beancount]
 trim_trailing_whitespace = false
 

commit 5e346365c6c178a454034da0fcc9eaa1a3c3e6f4

Handle typed metadata with value 0 correctly
Handle typed metadata with value 0 correctly.

This got broken in commit eedcb8b6 ("Handle empty metadata values").

Fixes #223

bin/ledger2beancount
@@ -528,7 +528,7 @@ sub handle_metadata($$$) {
 	    print_tags $depth, "^$metadata->{value}";
 	} else {
 	    # Metadata values can be empty
-	    my $value = $metadata->{value} ? $metadata->{value} : "";
+	    my $value = defined $metadata->{value} ? $metadata->{value} : "";
 	    if (defined($metadata->{typed})) {
 		$value = parse_ledger_value $value;
 	    } else {

tests/metadata.beancount
@@ -4,11 +4,13 @@
 ;   - Virtual posting in parentheses ignored
 ;   - Comment or metadata on virtual posting preserved as comment
 ;   - Virtual posting in bracket ignored (see convert_virtual option)
+;   - Commodity Avios renamed to AVIOS
 ;   - Metadata key 2012 renamed to x2012
 ;   - Metadata key 2012foo renamed to x2012foo
 ;   - Metadata key Date renamed to date
 ;   - Metadata key Invoice renamed to invoice
 ;   - Metadata key TEST1234TEST renamed to test1234test
+;   - Metadata key TP renamed to tp
 ;   - Metadata key Trip renamed to trip
 ;   - Metadata key a renamed to ax
 ;   - Metadata key test:test renamed to test-test
@@ -28,7 +30,11 @@
 1970-01-01 commodity EUR
 
 1970-01-01 open Assets:Cash
+1970-01-01 open Assets:Rewards
 1970-01-01 open Expenses:Food
+1970-01-01 open Income:Rewards
+
+1970-01-01 commodity AVIOS
 
 2018-03-19 * "Metadata after posting, with amount"
   test: "foo bar baz"
@@ -249,4 +255,9 @@
     foo: "bar"
   Assets:Cash                       -10.00 EUR
     xyz: "abc"
+
+2020-09-16 * "Typed data int 0"
+  tp: 0
+  Assets:Rewards                    3000 AVIOS
+  Income:Rewards                   -3000 AVIOS
 

tests/metadata.ledger
@@ -237,3 +237,8 @@ commodity EUR
     Assets:Cash                       -10.00 EUR
       ; xyz: abc
 
+2020-09-16 * Typed data int 0
+    ; TP:: 0
+    Assets:Rewards                    3000 Avios
+    Income:Rewards                   -3000 Avios
+