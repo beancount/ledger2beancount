beancount/ledger2beancount

commit fa4f09ee52232bbcb7e50b1bab9cfe552a3ec5a5

Preserve leading whitespace in posting comments

bin/ledger2beancount
@@ -60,7 +60,7 @@ my $commodity_quoted_RE = qr/(")(?:(?=(\\?))\g{-1}.)*?\g{-2}/;
 my $commodity_unquoted_RE = qr/(?!-)(?!")[^;\s0-9=)({}@*\/+,.-]+/;
 my $commodity_RE = qr/$commodity_quoted_RE|$commodity_unquoted_RE/;
 my $comment_top_level_RE = qr/[;#%|*]\s?(?<comment>.*?)/;
-my $comment_RE = qr/;\s*(?<comment>.*)/;
+my $comment_RE = qr/;(?<comment>.*)/;
 my $metadata_RE = qr/;\s*(?<key>[^\h:][^\h]*?):(?<typed>:)?(\s*$|\s+(?<value>.*))/;
 # Postings: this does not match everything, just ensures a line consists of
 # a posting
@@ -694,7 +694,7 @@ sub push_metadata($$$) {
 sub push_comment($$) {
     my ($depth, $comment) = @_;
 
-    push_line $depth, "; $comment";
+    push_line $depth, ";$comment";
 }
 
 
@@ -836,7 +836,7 @@ sub handle_hledger_tags($$$) {
 sub handle_comment($$$) {
     my ($depth, $l, $defer) = @_;
 
-    if ($config->{hledger} && $l =~ /;\s*(?<comment>.*?)(?<tags>$hledger_tags_RE)$/) {
+    if ($config->{hledger} && $l =~ /;(?<comment>.*?)(?<tags>$hledger_tags_RE)$/) {
 	handle_hledger_tags $depth, $+{tags}, $defer;
 	my $comment = $+{comment};
 	if ($+{tags}) {
@@ -846,7 +846,7 @@ sub handle_comment($$$) {
     } elsif ($l =~ /^$metadata_RE/) {  # metadata comment
 	handle_metadata $in_postings ? 2 : 1, \%+, $defer;
 	return;
-    } elsif ($l =~ /^;\s*\[(?<postdate>$date_RE)?(=(?<auxdate>$date_RE))?\](\s*;?\s*(?<comment>.*))/) {  # postdate/auxdate
+    } elsif ($l =~ /^;\s*\[(?<postdate>$date_RE)?(=(?<auxdate>$date_RE))?\](?<comment>.*)/) {  # postdate/auxdate
 	if ($+{postdate} && defined $config->{postdate_tag}) {
 	    push @cur_txn_meta, "$config->{postdate_tag}: " . pp_date $+{postdate}, 0;
 	}
@@ -854,8 +854,8 @@ sub handle_comment($$$) {
 	    push @cur_txn_meta, "$config->{auxdate_tag}: " . pp_date $+{auxdate}, 0;
 	}
 	return $+{comment};
-    } elsif ($l =~ /^$comment_RE\s+:$tags_RE:\s*$/
-	or $l =~ /^;\s+:$tags_RE:\s+(?<comment>.*)$/) {  # tags comment
+    } elsif ($l =~ /^;:$tags_RE:$/ or $l =~ /^$comment_RE\s+:$tags_RE:\s*$/
+	or $l =~ /^;\s*:$tags_RE:\s+(?<comment>.*)$/) {  # tags comment
 	# The $in_postings check is a workaround since there are no posting-level tags
 	if ($defer || $in_postings) {
 	    push @cur_txn_tags, split /:/, $+{tags};
@@ -1541,10 +1541,10 @@ sub process_txn(@) {
 	    }
 
 	    if ($i->[COMMENT]) {
-		my $comment = handle_comment $i->[DEPTH], "; $i->[COMMENT]", 1;
+		my $comment = handle_comment $i->[DEPTH], ";$i->[COMMENT]", 1;
 		if ($comment) {
 		    $l .= $i->[WHITESPACE] if !defined $i->[AMOUNT];
-		    $l .= " ; $comment" if $comment;
+		    $l .= " ;$comment" if $comment;
 		}
 	    }
 
@@ -1922,9 +1922,9 @@ while (@input) {
 	# account name, but such an account name is invalid so treat it
 	# as a comment.
 	if ($config->{hledger}) {
-	    ($account, $comment) = split /\s*;\s*/, $1, 2;
+	    ($account, $comment) = split /\s*;/, $1, 2;
 	} else {
-	    ($account, $comment) = split /\s\s+;\s*/, $1, 2;
+	    ($account, $comment) = split /\s\s+;/, $1, 2;
 	}
 	$account = map_account $account;
 	@stanza = read_stanza \@input;
@@ -1935,7 +1935,7 @@ while (@input) {
 	    next;
 	}
 	$account_declared{$account} = 1;
-	print_line $depth, sprintf "$config->{account_open_date} open %s%s", $account, $comment ? " ; $comment" : "";
+	print_line $depth, sprintf "$config->{account_open_date} open %s%s", $account, $comment ? " ;$comment" : "";
 	foreach $l (@stanza) {
 	    ($depth, $l) = strip_indentation $l;
 	    if ($l =~ /^note\s+(.*)/) {  # note
@@ -1952,7 +1952,7 @@ while (@input) {
 	my $comment = "";
 	if ($l =~ /^(?<commodity>$commodity_RE)\s*($comment_RE)?$/) {
 	    $commodity = map_commodity $+{commodity};
-	    $comment = " ; $+{comment}" if $+{comment};
+	    $comment = " ;$+{comment}" if $+{comment};
 	} elsif ($config->{hledger}) {
 	    # In hledger, a commodity directive can contain an amount
 	    # where the amount defines the format for this commodity
@@ -1961,7 +1961,7 @@ while (@input) {
 	    $commodity = $amount->[CURRENCY];
 	    $ledger_commodity_format{$commodity} = $amount->[NUMBER];
 	    if ($l =~ /^\s*$comment_RE/) {
-		$comment = " ; $+{comment}";
+		$comment = " ;$+{comment}";
             } elsif ($l) {
 		die "Unknown rest in commodity declaration for $commodity: $l";
             }
@@ -2053,7 +2053,7 @@ while (@input) {
 	    # You can have a comment on the same line as the payee
 	    my ($narration, $comment) = split /  +\s*;\s*|\s*\t+\s*;\s*/, $+{narration}, 2;
 	    $narration = "" if not $narration;
-	    $comment = handle_comment $depth + 1, "; $comment", 0 if $comment;
+	    $comment = handle_comment $depth + 1, ";$comment", 0 if $comment;
 	    push_header pp_date($+{date}, $year), $+{flag} ? $+{flag} : "txn", $narration;
 	    $cur_txn_header{comment} = $comment if $comment;
 	    push_metadata $depth + 1, $config->{auxdate_tag}, pp_date($+{auxdate}, $year) if defined $+{auxdate} && defined $config->{auxdate_tag};

tests/aux-date.ledger
@@ -21,7 +21,7 @@ commodity EUR
     Equity:Opening-Balance
 
 2018-03-18 * Test with posting aux date plus comment
-    Assets:Test                        10.00 EUR ; [=2018-03-13] ; foo
+    Assets:Test                        10.00 EUR ; [=2018-03-13] foo
     Equity:Opening-Balance
 
 2018-03-18 * Test with posting aux date after no amount

tests/comments.beancount
@@ -54,7 +54,7 @@
 
 2018-03-17 * "Tag and comment on same line"
   #tag
-  ; comment
+  ;comment
   Assets:Test                        10.00 EUR
   Equity:Opening-Balance            -10.00 EUR
 
@@ -82,5 +82,17 @@
 
 2019-01-25 * "You can put a comment on the same line as the payee" ; this is a comment
   Assets:Test                        10.00 EUR
+  Equity:Opening-Balance            -10.00 EUR
+
+;No space
+2020-10-01 * "Comment spacing"
+  ;No space
+  Assets:Test                        10.00 EUR ;No space
+  Equity:Opening-Balance            -10.00 EUR
+
+;  Two space
+2020-10-01 * "Comment spacing"
+  ;  Two space
+  Assets:Test                        10.00 EUR ;  Two space
   Equity:Opening-Balance            -10.00 EUR
 

tests/comments.ledger
@@ -99,3 +99,15 @@ end comment
     Assets:Test                        10.00 EUR
     Equity:Opening-Balance            -10.00 EUR
 
+;No space
+2020-10-01 * Comment spacing
+    ;No space
+    Assets:Test                        10.00 EUR;No space
+    Equity:Opening-Balance            -10.00 EUR
+
+;  Two space
+2020-10-01 * Comment spacing
+    ;  Two space
+    Assets:Test                        10.00 EUR;  Two space
+    Equity:Opening-Balance            -10.00 EUR
+

tests/hledger.beancount
@@ -160,7 +160,7 @@
   Assets:B
 
 2020-06-24 txn "Comment"
-  Assets:A                 10.00 EUR ; This is a comment which ends with a comma,
+  Assets:A                 10.00 EUR ;This is a comment which ends with a comma,
   Assets:B
 
 2020-07-26 txn "Tag test"
@@ -279,11 +279,11 @@
 
 1970-01-01 commodity BRL
 
-1970-01-01 commodity JPY ; comment
+1970-01-01 commodity JPY ;comment
 
 1970-01-01 commodity CZK
 
-1970-01-01 commodity HUF ; comment
+1970-01-01 commodity HUF ;comment
 
 1970-01-01 commodity ILS
 

tests/lots.beancount
@@ -234,7 +234,7 @@
   Equity:Opening-Balance    -9.00 GBP
 
 2018-04-22 * "Lot with comment"
-  Assets:Test               10.00 EUR {1.23 USD, 2018-03-16} ; comment
+  Assets:Test               10.00 EUR {1.23 USD, 2018-03-16} ;comment
   Equity:Opening-Balance   -12.30 USD
 
 2018-04-22 * "Lot with comment"

commit c5b17791c9523398a01b9e6351e2900415463d94

Avoid adding empty comments due to whitespace
Avoid adding empty comments that are a result from whitespace, either
trailing whitespace after certain elements or the leading whitespace
in certain elements (e.g. tags).

This issue was introduced in commit fa4f09e ("Preserve leading
whitespace in posting comments").

.editorconfig
@@ -35,6 +35,12 @@ indent_style = space
 [tests/accounts.ledger]
 trim_trailing_whitespace = false
 
+[tests/aux-date.ledger]
+trim_trailing_whitespace = false
+
+[tests/comments.ledger]
+trim_trailing_whitespace = false
+
 [tests/metadata.ledger]
 trim_trailing_whitespace = false
 

bin/ledger2beancount
@@ -853,7 +853,6 @@ sub handle_comment($$$) {
 	if ($+{auxdate} && defined $config->{auxdate_tag}) {
 	    push @cur_txn_meta, "$config->{auxdate_tag}: " . pp_date $+{auxdate}, 0;
 	}
-	return $+{comment};
     } elsif ($l =~ /^;:$tags_RE:$/ or $l =~ /^$comment_RE\s+:$tags_RE:\s*$/
 	or $l =~ /^;\s*:$tags_RE:\s+(?<comment>.*)$/) {  # tags comment
 	# The $in_postings check is a workaround since there are no posting-level tags
@@ -862,12 +861,12 @@ sub handle_comment($$$) {
 	} else {
 	    print_tags $depth, split /:/, $+{tags};
 	}
-	return $+{comment};
     } elsif ($l =~ /^$comment_RE/) {  # (every other) comment
-	return $+{comment};
+	# do nothing
     } else {
 	die "Can't process comment: $l";
     }
+    return $+{comment} if $+{comment} !~ /^\s*;?\s*$/;
 }
 
 

tests/aux-date.beancount
@@ -73,3 +73,8 @@
     aux-date: 2018-03-13
   Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * "Trailing whitespace"
+  Assets:Test                        10.00 EUR
+    date: 2020-09-30
+  Assets:Test                       -10.00 EUR
+

tests/aux-date.ledger
@@ -56,3 +56,7 @@ commodity EUR
     Assets:Test                                  ; [=2018-03-13] Foo Bar
     Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * Trailing whitespace
+    Assets:Test                        10.00 EUR ; [2020-09-30]      
+    Assets:Test                       -10.00 EUR
+

tests/comments.beancount
@@ -96,3 +96,7 @@
   Assets:Test                        10.00 EUR ;  Two space
   Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * "Empty comment, with trailing whitespace"
+  Assets:Test                        10.00 EUR
+  Assets:Test                       -10.00 EUR
+

tests/comments.ledger
@@ -111,3 +111,7 @@ end comment
     Assets:Test                        10.00 EUR;  Two space
     Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * Empty comment, with trailing whitespace
+    Assets:Test                        10.00 EUR   ;   
+    Assets:Test                       -10.00 EUR
+

tests/metadata.beancount
@@ -279,6 +279,12 @@
     test: "foo"
   Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * "Many spaces"
+  test: "foo bar baz"
+  Assets:Test                        10.00 EUR
+    test: "foo"
+  Equity:Opening-Balance            -10.00 EUR
+
 2020-10-01 * "Tabs"
   test: "foo bar baz"
   Assets:Test                        10.00 EUR

tests/metadata.ledger
@@ -257,6 +257,11 @@ commodity EUR
     Assets:Test                        10.00 EUR ;test: foo
     Equity:Opening-Balance            -10.00 EUR
 
+2020-10-01 * Many spaces
+    ;    test: foo bar baz
+    Assets:Test                        10.00 EUR   ;   test: foo
+    Equity:Opening-Balance            -10.00 EUR
+
 2020-10-01 * Tabs
     ;	test: foo bar baz
     Assets:Test                        10.00 EUR;	test: foo

tests/tags.beancount
@@ -100,6 +100,11 @@
     tags: "foo"
   Assets:Test                       -10.00 EUR
 
+2020-10-01 * "Many spaces before tag"
+  Assets:Test                        10.00 EUR
+    tags: "foo"
+  Assets:Test                       -10.00 EUR
+
 2020-10-01 * "Tab separated"
   ;	Comment
   Assets:Test                        10.00 EUR

tests/tags.ledger
@@ -92,6 +92,10 @@ commodity EUR
     Assets:Test                        10.00 EUR;:foo:
     Assets:Test                       -10.00 EUR
 
+2020-10-01 * Many spaces before tag
+    Assets:Test                        10.00 EUR   ;   :foo:
+    Assets:Test                       -10.00 EUR
+
 2020-10-01 * Tab separated
     ;	Comment
     Assets:Test                        10.00 EUR;	:foo: